a41e950c6cbd5f6410477ee95fc8629ac63f19d9
niosega <nicolas.bonfante@insa-lyon.fr>
2015-09-28 08:36:14 +0200

    creation de kmain

--- kmain.c
+++ kmain.c
@@ -1,5 +1,30 @@
-void kmain()
+void dummy()
 {
-  int a = 0;
-  a++;
+	return;
+}
+
+int div(int dividend, int divisor)
+{
+	int result = 0;
+	int remainder = dividend;
+	while (remainder >= divisor) {
+		result++;
+		remainder -= divisor;
+	}
+	return result;
+}
+
+int compute_volume(int rad)
+{
+	int rad3 = rad * rad * rad;
+	return div(4*355*rad3, 3*113);
+}
+
+int kmain( void )
+{
+	int radius = 5;
+	int volume;
+	dummy();
+	volume = compute_volume(radius);
+	return volume;
 }
