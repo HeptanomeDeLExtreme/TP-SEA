b25e16401927174a8dfc17fa2bd95cd953890d88
Chalumeau Marion <mchalumeau@if501-219-15.insa-lyon.fr>
2015-09-30 09:04:58 +0200

    Code fin chapitre 3

--- kmain.c
+++ kmain.c
@@ -1,5 +1,36 @@
-void kmain()
+void
+dummy()
 {
-  int a = 0;
-  a++;
+  return;
+}
+int
+div(int dividend, int divisor)
+{
+  int result = 0;
+  int remainder = dividend;
+  while (remainder >= divisor) {
+    result++;
+    remainder -= divisor;
+  }
+  return result;
+}
+int
+compute_volume(int rad)
+{
+  int rad3 = rad * rad * rad;
+  return div(4*355*rad3, 3*113);
+}
+int
+kmain( void )
+{
+  __asm("mrs r8, spsr");
+  __asm("cps 0b10000");
+  __asm("mrs r8, spsr");
+  __asm("bl dummy");
+  int radius = 5;
+  __asm("mov r2, %0" : : "r"(radius));
+  __asm("mov %0, r3" : "=r"(radius): :"r3");
+  int volume;
+  volume = compute_volume(radius);
+  return volume;
 }
