a33d8927be50ac3f3470a44d1cb3bf038a37310d
Omadz <ophelie.delsaux@hotmail.fr>
2015-10-04 15:27:30 +0200

    Bug question 4-17

--- kmain.c
+++ kmain.c
@@ -6,7 +6,8 @@ void kmain( void )
 	//__asm("mrs r0, spsr");
 	__asm("cps 0x10");
 	//sys_settime(18446744073709551613);
-	sys_settime(16);
+	uint64_t heure = 16;
+	sys_settime(heure);
 	uint64_t actual = sys_gettime();
 	actual++;
 }
