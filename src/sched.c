0718a6e0be63dabf431dce1f750530d56ae4ff31
Omadz <ophelie.delsaux@hotmail.fr>
2015-10-05 08:49:08 +0200

    MAJ compte-rendu-chapitre-4

--- src/sched.c
+++ src/sched.c
@@ -1,6 +1,7 @@
 #include "stdint.h"
 
 extern uint64_t* stackHead;
+struct pcb_s* current_process;
 
 void sys_yieldto(struct pcb_s* dest)
 {
@@ -11,6 +12,19 @@ void sys_yieldto(struct pcb_s* dest)
 
 void do_sys_yieldto()
 {
+	// Sauver le PCB
+	__asm("mov %0, r0" : : "r"(r[0]));
+	__asm("mov %0, r1" : : "r"(r[1]));
+	__asm("mov %0, r2" : : "r"(r[2]));
+	__asm("mov %0, r3" : : "r"(r[3]));
+	__asm("mov %0, r4" : : "r"(r[4]));
+	__asm("mov %0, r5" : : "r"(r[5]));
+	__asm("mov %0, r6" : : "r"(r[6]));
+	__asm("mov %0, r7" : : "r"(r[7]));
+	__asm("mov %0, r8" : : "r"(r[8]));
+	__asm("mov %0, r9" : : "r"(r[9]));
+	__asm("mov %0, r10" : : "r"(r[10]));
+	__asm("mov %0, r11" : : "r"(r[11]));
 	uint64_t dest;
 	dest = *(stackHead + 1);
 }
--- src/sched.h
+++ src/sched.h
@@ -3,6 +3,8 @@
 
 #include "stdint.h"
 
+#define NUMBER_REGISTER 12;
+
 /* User Side*/
 void sys_yieldto(struct pcb_s* dest);
 
@@ -11,7 +13,11 @@ void do_sys_yieldto();
 
 /* Structure definitions */
 struct pcb_s {
-
+	uint32_t reg[NUMBER_REGISTER];
+	uint32_t sp;
+	uint32_t lr;
+	uint32_t pc;
+	uint32_t cpsr;
 };
 
 
