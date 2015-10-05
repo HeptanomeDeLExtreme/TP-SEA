c2b350eb70c3941eb7eb2c3fa4e687fe59ff1752
niosega <nicolas.bonfante@insa-lyon.fr>
2015-10-05 08:17:04 +0200

    ohao

---
+++ src/sched.c
@@ -0,0 +1,42 @@
+#include"util.h"
+#include"syscall.h"
+#include"sched.h"
+
+struct pcb_s pcb1, pcb2;
+struct pcb_s *p1, *p2;
+
+void user_process_1()
+{
+	int
+	v1=5;
+	while(1)
+	{
+		v1++;
+		sys_yieldto(p2);
+	}
+}
+
+void user_process_2()
+{
+	int v2=-12;
+	while(1)
+	{
+		v2-=2;
+		sys_yieldto(p1);
+	}
+}
+
+void kmain(void)
+{
+	sched_init();
+	p1=&pcb1;
+	p2=&pcb2;
+	// initialize p1 and p2//
+	// [ ton code va la ]
+	__asm("cps 0x10");
+	// switch CPU to USER mode//
+	// **********************************************************************
+	sys_yieldto(p1);
+	// this is now unreachable
+	PANIC();
+}
---
+++ src/sched.h
@@ -0,0 +1,8 @@
+#ifndef ___SCHED_H___ 
+# define ___SCHED_H___
+
+#include "stdint.h"
+
+
+
+#endif
