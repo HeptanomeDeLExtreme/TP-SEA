46eee107a95148f7cf51dbdd79d75da5faa9857a
niosega <nicolas.bonfante@insa-lyon.fr>
2015-10-05 08:06:19 +0200

    Ben alors ?

---
+++ kmain_sys.c
@@ -0,0 +1,12 @@
+#include "src/syscall.h"
+#include "stdint.h"
+
+void kmain( void )
+{
+	//__asm("mrs r0, spsr");
+	__asm("cps 0x10");
+	//sys_settime(18446744073709551613);
+	uint64_t heure = 16;
+	sys_settime(heure);
+	uint64_t actual = sys_gettime();
+}
