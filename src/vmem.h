23ea27fd3cd86ce894c5f15975e277ffb097d871
mchalumeau <marion.chalumeau@insa-lyon.fr>
2015-11-30 12:01:13 +0100

    debut chapitre 9

---
+++ src/vmem.h
@@ -0,0 +1,10 @@
+
+final int PAGE_SIZE = 4096;
+final int SECON_LVL_TT_COUN = 8;
+final int SECON_LVL_TT_SIZE = 1048576;
+final int FIRST_LVL_TT_COUN = 5;
+final int FIRST_LVL_TT_SIZE = 16777216;
+
+void start_mmu_C();
+
+void configure_mmu_C();
