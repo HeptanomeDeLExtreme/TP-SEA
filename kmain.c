c0f04cef183396c759467e5e99b3de0447db2806
Omadz <ophelie.delsaux@hotmail.fr>
2015-12-16 09:30:35 +0100

    Affichage de la commande et du reste de la ligne de commande - TEST

--- kmain.c
+++ kmain.c
@@ -4,6 +4,7 @@
 #include "hw.h"
 #include "asm_tools.h"
 #include "fb.h"
+#include "string.h"
 
 void UsbInitialise();
 void KeyboardUpdate();
@@ -11,15 +12,60 @@ char KeyboardGetChar();
 
 char commandLine[256];
 int commandLineSize = 0;
+char command[256];
+int commandSize = 0;
+char parameters[256];
+int parametersSize = 0;
+
+// echo
+// music
+// currentTime
+// reboot
+
+void resetCommand()
+{
+	commandLineSize = 0;
+}
+
+void addCharacterInString(char c, char* str, int strSize)
+{
+	str[strSize] = c;
+	strSize++;
+}
+
+void commandProcess()
+{
+	int indexParameter = 0;
+	while(c != ' ')
+	{
+		addCharacterInString(commandLine[index], command, commandSize);
+	}
+	addCharacterInString('\0', command, commandSize);
+	
+	indexParameter = commandSize + 1;
+	while(c != '\0')
+	{
+		addCharacterInString(commandLine[indexParameter], parameters, parametersSize);
+		indexParameter;
+	}
+	addCharacterInString('\0', parameters, parametersSize);
+	
+	drawString(command, commandSize - 1); // Without /0
+	drawChar('\n');
+	drawString('ZBLAH !', 7);
+	drawString(parameters, parametersSize - 1);
+}
 
 void applyMethod()
 {
+	addCharacterInString('\0', commandLine, commandLineSize);
 	newLine();
-	for(int i = 0;i<commandLineSize;i++)
+	/*for(int i = 0;i<commandLineSize;i++)
 	{
 		drawChar(commandLine[i]);
-	}
-	commandLineSize = 0;
+	}*/
+	commandProcess();
+	resetCommand();
 	newLine();
 	drawChar('>');
 	drawChar(' ');
@@ -39,8 +85,7 @@ void keyboardLoop()
 				applyMethod();
 			}
 			else{
-				commandLine[commandLineSize] = c;
-				commandLineSize++;
+				addCharacterInString(c);
 				drawChar(c);
 			}
 		}
